0700dcf7143692b5f9541ccd74f76bee23e2afe8
Sean1840 <52455900+Sean1840@users.noreply.github.com>
2019-07-04 22:31:36 +0800

    Update Morse.ino

--- Morse/Morse.ino
+++ Morse/Morse.ino
@@ -5,11 +5,15 @@ char a[100];
 int i;
 void setup()
 {
-    gets(a);
+  Serial.begin(9600);
 }
 
 void loop()
 {
+    if(Serial.available()>0)
+  {
+    a=Serial.read();
+  }
     for(i=0;a[i]!='/0';i++)
     {
         if(a[i]=='a')
